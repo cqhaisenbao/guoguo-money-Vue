10be14911a59ddf881950dba553550820d8549e1
cqhaisenbao <fanyu9353@gmail.com>
2020-11-11 23:59:46 +0800

    修改 icon 配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  lintOnSave: false
-}
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons');
+    config.module
+        .rule('svg-sprite')
+        .test(/\.svg$/)
+        .include.add(dir).end()
+        .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end();
+
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}]);
+    // 这个目录下的文件的不再匹配svg规则
+    config.module.rule('svg').exclude.add(dir)
+  }
+};
