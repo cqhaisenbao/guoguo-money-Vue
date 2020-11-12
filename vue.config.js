31bd55b329588ff7a6b82b91c46c6f1386864cf8
cqhaisenbao <fanyu9353@gmail.com>
2020-11-12 11:06:21 +0800

    解决了svg自带fill属性的问题

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  chainWebpack: config => {
-    const dir = path.resolve(__dirname, 'src/assets/icons');
-    config.module
-        .rule('svg-sprite')
-        .test(/\.svg$/)
-        .include.add(dir).end()
-        .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end();
-
-    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}]);
-    // 这个目录下的文件的不再匹配svg规则
-    config.module.rule('svg').exclude.add(dir)
-  }
+    chainWebpack: config => {
+        const dir = path.resolve(__dirname, 'src/assets/icons');
+        config.module
+            .rule('svg-sprite')
+            .test(/\.svg$/)
+            .include.add(dir).end()
+            .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
+            .use('svgo-loader').loader('svgo-loader')
+            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end();
+        config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}]);
+        // 这个目录下的文件的不再匹配svg规则
+        config.module.rule('svg').exclude.add(dir)
+    }
 };
