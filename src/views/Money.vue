40abbd83a0efc0b9d239cd875d7019615bec0887
cqhaisenbao <fanyu9353@gmail.com>
2020-11-12 20:33:18 +0800

    基本完成 NumberPad 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,32 @@
 <template>
-    <Layout>
-        <p>Money.</p>
+    <Layout class-prefix="layout">
+        <NumberPad/>
+        <Types />
+        <Notes/>
+        <Tags :data-source.sync="tags"/>
     </Layout>
 </template>
 
 <script lang="ts">
+    import NumberPad from '@/components/Money/NumberPad.vue';
+    import Types from '@/components/Money/Types.vue';
+    import Notes from '@/components/Money/Notes.vue';
+    import Tags from '@/components/Money/Tags.vue';
+
     export default {
-        name: "Money",
-    }
+        name: 'Money',
+        components: {Tags, Notes, Types, NumberPad},
+        data() {
+            return {
+                tags: ['衣', '食', '住', '行', '彩票']
+            };
+        }
+    };
 </script>
+
+<style lang="scss">
+    .layout-content {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+</style>
