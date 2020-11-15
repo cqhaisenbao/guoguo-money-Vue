24aa1a5d389e060bf0a64696334865e9255e979a
cqhaisenbao <fanyu9353@gmail.com>
2020-11-15 10:51:43 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -32,12 +32,7 @@
         createTag() {
             const name = window.prompt('请输入标签名');
             if (name) {
-                const message = tagListModel.create(name);
-                if (message === 'duplicated') {
-                    window.alert('分类标签已经存在');
-                } else if (message === 'success') {
-                    window.alert('添加成功');
-                }
+                window.createTag(name);
             }
         }
     }
