adf1290c216fd731111f25172719d3a6cbb895ec
cqhaisenbao <fanyu9353@gmail.com>
2020-11-17 21:41:12 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,7 +15,6 @@
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
     import NumberPad from '@/components/Money/NumberPad.vue';
-    import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
     import recordTypeList from '@/constants/recordTypeList';
@@ -34,7 +33,7 @@
     // window.localStorage.setItem('version', '0.0.2');
 
     @Component({
-        components: {Tabs, Tags, FormItem, Types, NumberPad},
+        components: {Tabs, Tags, FormItem, NumberPad},
     })
     export default class Money extends Vue {
         get recordList() {
